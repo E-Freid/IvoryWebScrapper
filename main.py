f51ca5ffde33f898e06917eb150f01fe37ab51d6
Eli Freid <eli.freid@gmail.com>
2022-09-09 21:07:53 +0300

    added some code and created a new function

--- main.py
+++ main.py
@@ -13,20 +13,30 @@ driver = webdriver.Chrome(service=s)
 
 try:
     driver.get("https://www.ivory.co.il/catalog.php?act=cat&id=49787")
-    driver.implicitly_wait(10)
+    WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'single-item-wrapper')))
 except ConnectionError:
     print("Program can't establish connection to the site, exiting the program")
 
-items = driver.find_elements(By.CLASS_NAME, 'single-item-wrapper')
-for item in items:
+def get_item_info(url):
     try:
-        item_url = item.find_element(By.TAG_NAME,'a').get_attribute('href')
-        driver.get(item_url)
-        WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.ID,'titleProd')))
+        driver.get(url)
+        WebDriverWait(driver,10).until(EC.presence_of_element_located((By.ID,'productMainBlock')))
         item_name = driver.find_element(By.ID, 'titleProd').text
-        print(item_name)
+        item_id = driver.find_element(By.CLASS_NAME,'barcode-specific-area').text
+        try:
+            item_price = driver.find_element(By.CLASS_NAME,'print-no-eilat-price').text
+        except:
+            item_price = driver.find_element(By.CLASS_NAME, 'discount-price').text
+        print(f'{item_name}, {item_id}, {item_price}')
+    finally:
         driver.back()
-    except EC.StaleElementReferenceException:
-        print("Item not found")
+
+
+items = driver.find_elements(By.CLASS_NAME, 'single-item-wrapper')
+
+for item in items:
+    WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'single-item-wrapper')))
+    item_url = item.find_element(By.TAG_NAME,'a').get_attribute('href')
+    get_item_info(item_url)
 
 driver.quit()
