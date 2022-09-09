d42d6ec032519d7eaeffdac42a475ea6b33a995b
Eli Freid <eli.freid@gmail.com>
2022-09-09 20:33:56 +0300

    changed the code, seems to work now fine

--- main.py
+++ main.py
@@ -13,24 +13,20 @@ driver = webdriver.Chrome(service=s)
 
 try:
     driver.get("https://www.ivory.co.il/catalog.php?act=cat&id=49787")
-except:
+    driver.implicitly_wait(10)
+except ConnectionError:
     print("Program can't establish connection to the site, exiting the program")
 
-try:
-    items_div = driver.find_element(By.XPATH, '//*[@id="productPage"]/div[3]/div[2]/div/div[1]/div[2]')
-    items = items_div.find_elements(By.CLASS_NAME, 'single-item-wrapper')
-    for item in items:
-        WebDriverWait(driver,10).until(EC.element_to_be_clickable(item))  # need to fix this bug
-        item.click()
-        try:
-            WebDriverWait(driver,10).until(EC.presence_of_element_located((By.ID,'titleProd')))
-            item_name = driver.find_element(By.ID, 'titleProd').text
-            print(item_name)
-            item_id = driver.find_element(By.XPATH, '//*[@id="productMainBlock"]/div[1]/div[3]/span/span').text
-            print(item_id)
-            item_price = driver.find_element(By.ID, 'pricetotalitemjs').text
-            print(item_price)
-        finally:
-            driver.back()
-finally:
-    driver.quit()
+items = driver.find_elements(By.CLASS_NAME, 'single-item-wrapper')
+for item in items:
+    try:
+        item_url = item.find_element(By.TAG_NAME,'a').get_attribute('href')
+        driver.get(item_url)
+        WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.ID,'titleProd')))
+        item_name = driver.find_element(By.ID, 'titleProd').text
+        print(item_name)
+        driver.back()
+    except EC.StaleElementReferenceException:
+        print("Item not found")
+
+driver.quit()
