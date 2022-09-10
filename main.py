5b4191f1fb6f9aa3ea700ae515dc9ca2bbb76a96
Eli Freid <eli.freid@gmail.com>
2022-09-10 21:46:41 +0300

    program now gets info from all the items in the renewed items catalog

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ import time
 
 PATH = "/usr/local/bin/chromedriver"
 s = Service(PATH)
-
 driver = webdriver.Chrome(service=s)
 
-try:
-    driver.get("https://www.ivory.co.il/catalog.php?act=cat&id=49787")
-    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'single-item-wrapper')))
-except ConnectionError:
-    print("Program can't establish connection to the site, exiting the program")
+
+def initialize_driver():
+    try:
+        driver.get("https://www.ivory.co.il/catalog.php?act=cat&id=49787")
+        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'single-item-wrapper')))
+    except ConnectionError:
+        print("Program can't establish connection to the site, exiting the program")
+        driver.quit()
+        exit(1)
 
 
 def get_item_info(url):
@@ -36,21 +39,41 @@ def get_item_info(url):
         return None
 
 
-def get_items_info(items_url):
-    for url in items_url:
-        new_item_idea = get_item_info(url)
+def get_items_info(items_urls):
+    for url in items_urls:
+        new_item_id = get_item_info(url)
         # get new_item_info function
 
 
-def get_item_urls():
+def get_item_urls_from_single_page(items_url_arr, url):
+    try:
+        driver.get(url)
+        WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CLASS_NAME,'product-anchor')))
+        items = driver.find_elements(By.CLASS_NAME, 'product-anchor')
+        for item in items:
+            items_url_arr.append(item.get_attribute('href'))
+    except ConnectionError:
+        print("failed to connect to the page")
+
+
+def get_items_urls():
+    pages_urls = []
     items_url = []
-    items = driver.find_elements(By.CLASS_NAME, 'product-anchor')
-    for item in items:
-        items_url.append(item.get_attribute('href'))
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'pagesList')))
+    pages_bar = driver.find_element(By.ID, 'pagesList')
+    pages = pages_bar.find_elements(By.TAG_NAME, 'a')
+    pages.pop()
+    pages.pop()
+    pages_urls.append(driver.current_url)
+    for page in pages:
+        pages_urls.append(page.get_attribute('href'))
+    for url in pages_urls:
+        get_item_urls_from_single_page(items_url, url)
     return items_url
 
 
 if __name__ == '__main__':
-    items_url = get_item_urls()
+    initialize_driver()
+    items_url = get_items_urls()
     get_items_info(items_url)
     driver.quit()
